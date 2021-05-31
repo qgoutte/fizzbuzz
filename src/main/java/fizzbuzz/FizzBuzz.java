c979889bb949446bdedf916d334f91f76aeaec1c
qgoutte <quentingoutte@gmail.com>
2021-05-31 21:43:23 +0200

    init functions

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -5,27 +5,12 @@ public class FizzBuzz {
 		System.out.println("Hello World");
 	}
 
-	private boolean isFizz(int i) {
-		return true;
+	public void fizzbuzzStage1() {
+
 	}
 
-	private boolean isBuzz(int i) {
-		return true;
+	public void fizzbuzzStage2() {
+
 	}
 
-	private void printResult(int i, boolean isFizz, boolean isBuzz) {
-		// TODO : print real result
-	}
-
-	private void printValue(int i) {
-		System.out.println(i);
-	}
-
-	private void printFizz() {
-		System.out.println("Fizz");
-	}
-
-	private void printBuzz() {
-		System.out.println("Buzz");
-	}
 }
