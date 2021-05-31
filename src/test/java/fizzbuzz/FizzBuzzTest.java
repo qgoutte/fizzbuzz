76f8cd1ab677ee6e9cb6eb8dbee1259d5815ff0e
qgoutte <quentingoutte@gmail.com>
2021-05-31 22:03:32 +0200

    merge branch stage-1

---
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,18 @@
+package fizzbuzz;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void fizzBuzzStage1() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+
+	}
+
+	@Test
+	public void fizzBuzzStage2() {
+
+	}
+
+}
