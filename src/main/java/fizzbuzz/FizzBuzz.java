e87ce6153f8191463230e2bd58dfe991724ba520
qgoutte <quentingoutte@gmail.com>
2021-05-31 21:58:44 +0200

    FizzBuzz stage 1

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,15 +1,23 @@
 package fizzbuzz;
 
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		fizzbuzzStage1();
 	}
 
-	public void fizzbuzzStage1() {
-
+	public static void fizzbuzzStage1() {
+		IntStream.rangeClosed(1, 100)
+				.mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz" : "Fizz") : (i % 5 == 0 ? "Buzz" : i))
+				.forEach(System.out::println);
 	}
 
 	public void fizzbuzzStage2() {
+
+	}
+
+	public void check(int i) {
 
 	}
 
